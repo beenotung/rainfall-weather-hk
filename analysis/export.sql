95a88450c100bb5737ebd1d977355b77bd86b26e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-21 17:55:16 +0800

    chore: remove unrelated time_id and date_id from export.sql

--- analysis/export.sql
+++ analysis/export.sql
@@ -1,8 +1,6 @@
 select
   rainfall.amount as rainfall_amount
 , rainfall.district_id
-, rainfall.time_id
-, rainfall.date_id
 , rainfall.id as rainfall_id
 , district.name as district_name
 , time.hour as time_hour
